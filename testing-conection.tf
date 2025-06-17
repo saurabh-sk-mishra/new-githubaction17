bc9f39bc4020fbd05c8dc5414315a6b359539ea8
saurabh-sk-mishra <skvedicestates@gmail.com>
2025-06-17 22:28:26 +0530

    Update testing-conection.tf

--- testing-conection.tf
+++ testing-conection.tf
@@ -1,18 +1,1 @@
-jobs:
-  terraform:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v3
 
-    - name: Setup Terraform
-      uses: hashicorp/setup-terraform@v3
-      with:
-        terraform_version: 1.6.0
-
-    - name: Terraform Init
-      run: terraform init
-      working-directory: ./infra  # <-- point to your config files
-
-    - name: Terraform Plan
-      run: terraform plan -input=false
-      working-directory: ./infra  # <-- same here
